61e68056371a88049af84af65d30a23cac6c2702
Rizadh Nizam <me@rizadh.com>
2019-04-21 13:26:38 -0400

    Improve render performance of WaypointItem
    Use memoized callbacks with useCallback().

--- client/src/components/WaypointItem.tsx
+++ client/src/components/WaypointItem.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useCallback } from 'react'
 import { Draggable, DraggableProvided, DraggableStateSnapshot } from 'react-beautiful-dnd'
+import { useInputField } from '../hooks/useInputField'
 import { PlaceFetchResult, RouteFetchResult, Waypoint } from '../redux/state'
 import { isValidAddress } from '../redux/validator'
 
@@ -16,19 +17,19 @@ type WaypointItemProps = {
 }
 
 export const WaypointItem = (props: WaypointItemProps) => {
-    const [waypointFieldValue, setWaypointFieldValue] = useState(props.waypoint.address)
+    const {
+        value: waypointFieldValue,
+        setValue: setWaypointFieldValue,
+        changeHandler: handleWaypointFieldValueChange,
+        keyPressHandler: handleWaypointFieldKeyPress,
+    } = useInputField(
+        props.waypoint.address,
+        () => fieldWasEdited && isValidAddress(waypointFieldValue) && props.setAddress(waypointFieldValue),
+    )
 
-    const handleWaypointFieldValueChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-        setWaypointFieldValue(e.currentTarget.value)
-
-    const handleWaypointFieldKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter' && isValidAddress(waypointFieldValue)) {
-            props.setAddress(waypointFieldValue)
-            e.currentTarget.blur()
-        }
-    }
-
-    const resetWaypointField = () => setWaypointFieldValue(props.waypoint.address)
+    const resetWaypointField = useCallback(() => setWaypointFieldValue(props.waypoint.address), [
+        props.waypoint.address,
+    ])
 
     const fieldWasEdited = waypointFieldValue !== props.waypoint.address
 
@@ -42,47 +43,45 @@ export const WaypointItem = (props: WaypointItemProps) => {
         (props.incomingRouteFetchResult && props.incomingRouteFetchResult.status === 'FAILED') ||
         (props.outgoingRouteFetchResult && props.outgoingRouteFetchResult.status === 'FAILED')
 
-    const renderItem = (provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-        <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            className={'input-row ' + (props.waypoint.isSelected ? 'waypoint-item-selected' : '')}
-        >
-            <button onClick={props.deleteWaypoint} className="btn btn-sm btn-danger">
-                <i className="fas fa-trash-alt" />
-            </button>
-            <input
-                className={'form-control ' + (props.waypoint.isSelected ? 'text-primary' : '')}
-                value={waypointFieldValue}
-                onChange={handleWaypointFieldValueChange}
-                onKeyPress={handleWaypointFieldKeyPress}
-                disabled={props.isBeingDragged && !snapshot.isDragging}
-            />
-            {fieldWasEdited && (
-                <button onClick={resetWaypointField} className="btn btn-secondary">
-                    <i className="fas fa-undo-alt" />
-                </button>
-            )}
-            <span>{props.index + 1}</span>
-            {fetchFailed && (
-                <span className="text-danger">
-                    <i className="fas fa-exclamation-circle" />
-                </span>
-            )}
-            {fetchIsInProgress && (
-                <span className="text-muted">
-                    <i className="fas fa-circle-notch fa-spin" />
-                </span>
-            )}
-            <span onClick={props.itemWasClicked} {...provided.dragHandleProps}>
-                <i className="fas fa-grip-lines-vertical" />
-            </span>
-        </div>
-    )
-
     return (
         <Draggable index={props.index} draggableId={props.waypoint.uuid}>
-            {renderItem}
+            {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    className={'input-row ' + (props.waypoint.isSelected ? 'waypoint-item-selected' : '')}
+                >
+                    <button onClick={props.deleteWaypoint} className="btn btn-sm btn-danger">
+                        <i className="fas fa-trash-alt" />
+                    </button>
+                    <input
+                        className="form-control"
+                        value={waypointFieldValue}
+                        onChange={handleWaypointFieldValueChange}
+                        onKeyPress={handleWaypointFieldKeyPress}
+                        disabled={props.isBeingDragged && !snapshot.isDragging}
+                    />
+                    {fieldWasEdited && (
+                        <button onClick={resetWaypointField} className="btn btn-secondary">
+                            <i className="fas fa-undo-alt" />
+                        </button>
+                    )}
+                    <span>{props.index + 1}</span>
+                    {fetchFailed && (
+                        <span className="text-danger">
+                            <i className="fas fa-exclamation-circle" />
+                        </span>
+                    )}
+                    {fetchIsInProgress && (
+                        <span className="text-muted">
+                            <i className="fas fa-circle-notch fa-spin" />
+                        </span>
+                    )}
+                    <span onClick={props.itemWasClicked} {...provided.dragHandleProps}>
+                        <i className="fas fa-grip-lines-vertical" />
+                    </span>
+                </div>
+            )}
         </Draggable>
     )
 }
