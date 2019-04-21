16676ca84e46ca66c343a425109c7faf357cb3bf
Rizadh Nizam <me@rizadh.com>
2019-04-21 14:03:00 -0400

    Move more logic into WaypointItem using context

--- client/src/components/WaypointItem.tsx
+++ client/src/components/WaypointItem.tsx
@@ -1,57 +1,92 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useContext } from 'react'
 import { Draggable, DraggableProvided, DraggableStateSnapshot } from 'react-beautiful-dnd'
+import { AppStateContext } from '../context/AppStateContext'
 import { useInputField } from '../hooks/useInputField'
-import { PlaceFetchResult, RouteFetchResult, Waypoint } from '../redux/state'
+import {
+    deleteWaypoint as deleteWaypointActionCreator,
+    selectWaypoint,
+    selectWaypointRange,
+    setAddress as setAddressActionCreator,
+    toggleWaypointSelection,
+} from '../redux/actions'
+import { RouteFetchResult } from '../redux/state'
 import { isValidAddress } from '../redux/validator'
 
 type WaypointItemProps = {
     index: number;
-    waypoint: Waypoint;
-    setAddress: (address: string) => void;
-    deleteWaypoint: () => void;
-    placeFetchResult?: PlaceFetchResult;
-    outgoingRouteFetchResult?: RouteFetchResult;
-    incomingRouteFetchResult?: RouteFetchResult;
     isBeingDragged: boolean;
-    itemWasClicked: (e: React.MouseEvent) => void;
 }
 
 export const WaypointItem = (props: WaypointItemProps) => {
+    const { index, isBeingDragged } = props
+
+    const {
+        state: { waypoints, fetchedPlaces, fetchedRoutes },
+        dispatch,
+    } = useContext(AppStateContext)
+    const waypoint = waypoints[index]
+
     const {
         value: waypointFieldValue,
         setValue: setWaypointFieldValue,
         changeHandler: handleWaypointFieldValueChange,
         keyPressHandler: handleWaypointFieldKeyPress,
     } = useInputField(
-        props.waypoint.address,
-        () => fieldWasEdited && isValidAddress(waypointFieldValue) && props.setAddress(waypointFieldValue),
+        waypoint.address,
+        () => fieldWasEdited && isValidAddress(waypointFieldValue) && setAddress(waypointFieldValue),
+    )
+    const fieldWasEdited = waypointFieldValue !== waypoint.address
+
+    const setAddress = useCallback(newAddress => dispatch(setAddressActionCreator(index, newAddress)), [index])
+    const deleteWaypoint = useCallback(() => dispatch(deleteWaypointActionCreator(index)), [index])
+    const resetWaypointField = useCallback(() => setWaypointFieldValue(waypoint.address), [waypoint.address])
+    const routeFetchResult = useCallback(
+        (origin: string, destination: string): RouteFetchResult | undefined => {
+            const routesFromOrigin = fetchedRoutes.get(origin)
+
+            return routesFromOrigin ? routesFromOrigin.get(destination) : undefined
+        },
+        [fetchedRoutes],
     )
 
-    const resetWaypointField = useCallback(() => setWaypointFieldValue(props.waypoint.address), [
-        props.waypoint.address,
-    ])
+    const placeFetchResult = fetchedPlaces.get(waypoint.address)
 
-    const fieldWasEdited = waypointFieldValue !== props.waypoint.address
+    const incomingRouteFetchResult =
+        index === 0 && routeFetchResult(waypoints[index - 1].address, waypoints[index].address)
+
+    const outgoingRouteFetchResult =
+        index === waypoints.length - 1 && routeFetchResult(waypoints[index].address, waypoints[index + 1].address)
+
+    const itemWasClicked = (e: React.MouseEvent) => {
+        if (e.shiftKey) {
+            e.preventDefault()
+            dispatch(selectWaypointRange(index))
+        } else if (e.ctrlKey || e.metaKey) {
+            dispatch(toggleWaypointSelection(index))
+        } else {
+            dispatch(selectWaypoint(index))
+        }
+    }
 
     const fetchIsInProgress =
-        (props.placeFetchResult && props.placeFetchResult.status === 'IN_PROGRESS') ||
-        (props.incomingRouteFetchResult && props.incomingRouteFetchResult.status === 'IN_PROGRESS') ||
-        (props.outgoingRouteFetchResult && props.outgoingRouteFetchResult.status === 'IN_PROGRESS')
+        (placeFetchResult && placeFetchResult.status === 'IN_PROGRESS') ||
+        (incomingRouteFetchResult && incomingRouteFetchResult.status === 'IN_PROGRESS') ||
+        (outgoingRouteFetchResult && outgoingRouteFetchResult.status === 'IN_PROGRESS')
 
     const fetchFailed =
-        (props.placeFetchResult && props.placeFetchResult.status === 'FAILED') ||
-        (props.incomingRouteFetchResult && props.incomingRouteFetchResult.status === 'FAILED') ||
-        (props.outgoingRouteFetchResult && props.outgoingRouteFetchResult.status === 'FAILED')
+        (placeFetchResult && placeFetchResult.status === 'FAILED') ||
+        (incomingRouteFetchResult && incomingRouteFetchResult.status === 'FAILED') ||
+        (outgoingRouteFetchResult && outgoingRouteFetchResult.status === 'FAILED')
 
     return (
-        <Draggable index={props.index} draggableId={props.waypoint.uuid}>
+        <Draggable index={index} draggableId={waypoint.uuid}>
             {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
                 <div
                     ref={provided.innerRef}
                     {...provided.draggableProps}
-                    className={'input-row ' + (props.waypoint.isSelected ? 'waypoint-item-selected' : '')}
+                    className={'input-row ' + (waypoint.isSelected ? 'waypoint-item-selected' : '')}
                 >
-                    <button onClick={props.deleteWaypoint} className="btn btn-sm btn-danger">
+                    <button onClick={deleteWaypoint} className="btn btn-sm btn-danger">
                         <i className="fas fa-trash-alt" />
                     </button>
                     <input
@@ -59,14 +94,14 @@ export const WaypointItem = (props: WaypointItemProps) => {
                         value={waypointFieldValue}
                         onChange={handleWaypointFieldValueChange}
                         onKeyPress={handleWaypointFieldKeyPress}
-                        disabled={props.isBeingDragged && !snapshot.isDragging}
+                        disabled={isBeingDragged && !snapshot.isDragging}
                     />
                     {fieldWasEdited && (
                         <button onClick={resetWaypointField} className="btn btn-secondary">
                             <i className="fas fa-undo-alt" />
                         </button>
                     )}
-                    <span>{props.index + 1}</span>
+                    <span>{index + 1}</span>
                     {fetchFailed && (
                         <span className="text-danger">
                             <i className="fas fa-exclamation-circle" />
@@ -77,7 +112,7 @@ export const WaypointItem = (props: WaypointItemProps) => {
                             <i className="fas fa-circle-notch fa-spin" />
                         </span>
                     )}
-                    <span onClick={props.itemWasClicked} {...provided.dragHandleProps}>
+                    <span onClick={itemWasClicked} {...provided.dragHandleProps}>
                         <i className="fas fa-grip-lines-vertical" />
                     </span>
                 </div>
--- client/src/components/WaypointList.tsx
+++ client/src/components/WaypointList.tsx
@@ -1,42 +1,15 @@
 import React, { useContext } from 'react'
 import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd'
 import { AppStateContext } from '../context/AppStateContext'
-import {
-    deleteWaypoint,
-    moveSelectedWaypoints,
-    moveWaypoint,
-    selectWaypoint,
-    selectWaypointRange,
-    setAddress,
-    toggleWaypointSelection,
-} from '../redux/actions'
-import { PlaceFetchResult, RouteFetchResult } from '../redux/state'
+import { moveSelectedWaypoints, moveWaypoint } from '../redux/actions'
+import { Waypoint } from '../redux/state'
 import { WaypointItem } from './WaypointItem'
 
 export const WaypointList = () => {
     const {
-        state: { waypoints, fetchedPlaces, fetchedRoutes },
+        state: { waypoints },
         dispatch,
     } = useContext(AppStateContext)
-    const placeFetchResult = (address: string): PlaceFetchResult | undefined => fetchedPlaces.get(address)
-
-    const incomingRouteFetchResult = (index: number): RouteFetchResult | undefined => {
-        if (index === 0) return
-
-        return routeFetchResult(waypoints[index - 1].address, waypoints[index].address)
-    }
-
-    const outgoingRouteFetchResult = (index: number): RouteFetchResult | undefined => {
-        if (index === waypoints.length - 1) return
-
-        return routeFetchResult(waypoints[index].address, waypoints[index + 1].address)
-    }
-
-    const routeFetchResult = (origin: string, destination: string): RouteFetchResult | undefined => {
-        const routesFromOrigin = fetchedRoutes.get(origin)
-
-        return routesFromOrigin ? routesFromOrigin.get(destination) : undefined
-    }
 
     const onDragEnd = (result: DropResult) => {
         if (!result.destination) return
@@ -49,34 +22,16 @@ export const WaypointList = () => {
         }
     }
 
-    const itemWasClicked = (index: number) => (e: React.MouseEvent) => {
-        if (e.shiftKey) {
-            e.preventDefault()
-            dispatch(selectWaypointRange(index))
-        } else if (e.ctrlKey || e.metaKey) {
-            dispatch(toggleWaypointSelection(index))
-        } else {
-            dispatch(selectWaypoint(index))
-        }
-    }
-
     return (
         <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="waypointlist">
                 {(provided, snapshot) => (
                     <div ref={provided.innerRef} {...provided.droppableProps}>
-                        {waypoints.map((waypoint, index) => (
+                        {waypoints.map((waypoint: Waypoint, index: number) => (
                             <WaypointItem
                                 key={waypoint.uuid}
                                 index={index}
-                                waypoint={waypoint}
                                 isBeingDragged={snapshot.isDraggingOver && waypoint.isSelected}
-                                placeFetchResult={placeFetchResult(waypoint.address)}
-                                outgoingRouteFetchResult={outgoingRouteFetchResult(index)}
-                                incomingRouteFetchResult={incomingRouteFetchResult(index)}
-                                itemWasClicked={itemWasClicked(index)}
-                                deleteWaypoint={() => dispatch(deleteWaypoint(index))}
-                                setAddress={newAddress => dispatch(setAddress(index, newAddress))}
                             />
                         ))}
                         {provided.placeholder}
