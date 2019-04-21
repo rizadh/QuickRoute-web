057b767a3657ef2d556f2f365e2448c71a19d335
Rizadh Nizam <me@rizadh.com>
2019-04-21 11:31:09 -0400

    Update app version number

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
 
-export const appVersion = 'v2019.04.18'
+export const appVersion = 'v2019.04.21'
 
 // tslint:disable-next-line:no-var-requires
 require('babel-polyfill')
