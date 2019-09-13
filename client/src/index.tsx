355cdef2739c46e9cb1f0ba459ecb265a40b958d
Rizadh Nizam <me@rizadh.com>
2019-09-12 23:41:27 -0400

    Bump version number

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
 
-export const appVersion = 'v2019.05.07'
+export const appVersion = 'v2019.09.12'
 export const apiPrefix = 'https://rizadh.com/route-planner/'
 
 // tslint:disable-next-line:no-var-requires
