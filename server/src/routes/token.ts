e9f47f23189668192387befca47309d4ba4f9c8d
Rizadh Nizam <me@rizadh.com>
2019-12-21 01:47:13 -0500

    Add PEM boundaries for MapKit key in code

--- server/src/routes/token.ts
+++ server/src/routes/token.ts
@@ -7,7 +7,7 @@ if (!process.env.MAPKIT_KEY_ID) throw new Error('MAPKIT_KEY_ID not provided')
 const { MAPKIT_KEY, MAPKIT_TEAM_ID, MAPKIT_KEY_ID } = process.env
 
 const tokensRoute: IMiddleware = ctx => {
-    const key = MAPKIT_KEY
+    const key = `-----BEGIN PRIVATE KEY-----\n${MAPKIT_KEY}\n-----END PRIVATE KEY-----`
     const payload = {
         iss: MAPKIT_TEAM_ID,
         iat: Math.floor(Date.now() / 1000),
