ee35f0b7d4985ac8fcdf01c89d8ad218aaf33688
Rizadh Nizam <me@rizadh.com>
2020-01-07 00:06:59 -0500

    Use object destructuring to cleanup reducer

--- src/redux/reducers/waypointsReducer.ts
+++ src/redux/reducers/waypointsReducer.ts
@@ -29,29 +29,31 @@ export const waypointsReducer: AppReducer<WaypointsState> = produce(
                 waypoints.list.splice(action.targetIndex, 0, removed)
                 break
             case 'MOVE_SELECTED_WAYPOINTS':
-                const lowestIndex = waypoints.list.findIndex(waypoint => waypoints.selected.has(waypoint.uuid))
+                const lowestIndex = waypoints.list.findIndex(({ uuid }) => waypoints.selected.has(uuid))
                 const partitionIndex = lowestIndex < action.index ? action.index + 1 : action.index
 
                 const waypointsBeforePartition = waypoints.list.filter(
-                    (waypoint, index) => !waypoints.selected.has(waypoint.uuid) && index < partitionIndex,
+                    ({ uuid }, index) => !waypoints.selected.has(uuid) && index < partitionIndex,
                 )
                 const movedWaypoints = waypoints.list.filter(waypoint => waypoints.selected.has(waypoint.uuid))
                 const waypointsAfterPartition = waypoints.list.filter(
-                    (waypoint, index) => !waypoints.selected.has(waypoint.uuid) && index >= partitionIndex,
+                    ({ uuid }, index) => !waypoints.selected.has(uuid) && index >= partitionIndex,
                 )
 
                 waypoints.list = [...waypointsBeforePartition, ...movedWaypoints, ...waypointsAfterPartition]
 
                 break
             case 'SELECT_WAYPOINT': {
-                const waypoint = waypoints.list[action.index]
+                const { uuid } = waypoints.list[action.index]
 
-                const selectedCurrentWaypoints = waypoints.list.filter(w => waypoints.selected.has(w.uuid))
-                if (selectedCurrentWaypoints.length === 1 && waypoints.selected.has(waypoint.uuid)) {
+                const selectedCurrentWaypoints = waypoints.list.filter(({ uuid: currenUuid }) =>
+                    waypoints.selected.has(currenUuid),
+                )
+                if (selectedCurrentWaypoints.length === 1 && waypoints.selected.has(uuid)) {
                     // TODO: Can be cleared directly with Immer v4.0
                     waypoints.selected = new Set()
                 } else {
-                    waypoints.selected = new Set([waypoint.uuid])
+                    waypoints.selected = new Set([uuid])
                 }
 
                 waypoints.lastSelected = waypoints.list[action.index].uuid
@@ -61,15 +63,15 @@ export const waypointsReducer: AppReducer<WaypointsState> = produce(
                 // TODO: Can be set directly with Immer v4.0
                 const newSelectedWaypoints = new Set<string>(waypoints.selected as Set<string>)
 
-                const waypoint = waypoints.list[action.index]
+                const { uuid } = waypoints.list[action.index]
 
-                if (waypoints.selected.has(waypoint.uuid)) {
-                    newSelectedWaypoints.delete(waypoint.uuid)
+                if (waypoints.selected.has(uuid)) {
+                    newSelectedWaypoints.delete(uuid)
                 } else {
-                    newSelectedWaypoints.add(waypoint.uuid)
+                    newSelectedWaypoints.add(uuid)
                 }
 
-                waypoints.lastSelected = waypoint.uuid
+                waypoints.lastSelected = uuid
 
                 waypoints.selected = newSelectedWaypoints
                 break
