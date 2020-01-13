56b2d18e49f35a325265a3676649922181300837
Rizadh Nizam <me@rizadh.com>
2020-01-13 14:28:30 -0500

    Enable hot module reload for React components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,5 @@
+import { hot } from 'react-hot-loader'
+
 import React from 'react'
 import { Provider } from 'react-redux'
 import { ProgressBar } from '../components/ProgressBar'
@@ -6,11 +8,11 @@ import { MapButtons } from './MapButtons'
 import { MapView } from './MapView'
 import { WaypointEditor } from './WaypointEditor'
 
-export const App = () => (
+export const App = hot(module)(() => (
     <Provider store={store}>
         <MapView />
         <ProgressBar />
         <MapButtons />
         <WaypointEditor />
     </Provider>
-)
+))
