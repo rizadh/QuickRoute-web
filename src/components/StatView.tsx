51fccb259aa3bb7fc956a2a1512dd374f01bc176
Rizadh Nizam <me@rizadh.com>
2020-01-20 17:43:07 -0500

    Use styled-components to style StatView

--- src/components/StatView.tsx
+++ src/components/StatView.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 
-import './StatView.scss'
+import styled from 'styled-components'
 
 type StatViewProps = {
     title: string;
     value: string;
 }
 
+const Title = styled.span`
+    color: var(--app-primary-text-color);
+    font-weight: 600;
+`
+
+const Value = styled.span`
+    color: var(--app-secondary-text-color);
+`
+
+const Spacer = styled.div`
+    width: 8px;
+    display: inline-block;
+`
+
 export const StatView = (props: StatViewProps) => (
-    <div className="statview">
-        <span>{props.title}</span>
-        <div className="spacer" />
-        {props.value}
+    <div>
+        <Title>{props.title}</Title>
+        <Spacer />
+        <Value>{props.value}</Value>
     </div>
 )
