2985bfecfdf1cd1331ffef200e6a38aa7929408c
Rizadh Nizam <me@rizadh.com>
2020-01-20 19:08:13 -0500

    Re-implement ProgressBar with styled-components

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,27 +1,39 @@
-import { Line } from 'rc-progress'
 import React from 'react'
 import { shallowEqual, useSelector } from 'react-redux'
+import styled from 'styled-components'
 import { useDarkMode } from '../hooks/useDarkMode'
 import { routeInformation } from '../redux/selectors'
 
-import './ProgressBar.scss'
+const StyledProgressBar = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 4px;
+`
+
+const Track = styled.div<{ darkMode: boolean }>`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: ${({ darkMode }) => (darkMode ? 'rgba(10, 132, 255, 0.2)' : 'rgb(0, 122, 255, 0.2)')};
+`
+
+const Bar = styled.div<{ darkMode: boolean; progress: number }>`
+    position: absolute;
+    width: ${({ progress }) => progress * 100}%;
+    height: 100%;
+    background-color: ${({ darkMode }) => (darkMode ? 'rgba(10, 132, 255)' : 'rgb(0, 122, 255)')};
+`
 
 export const ProgressBar = () => {
     const darkMode = useDarkMode()
     const currentRouteInformation = useSelector(routeInformation, shallowEqual)
 
-    if (currentRouteInformation.status === 'FETCHING') {
-        return (
-            <div id="progress-bar">
-                <Line
-                    percent={currentRouteInformation.progress * 100}
-                    trailColor={darkMode ? 'rgba(10, 132, 255, 0.2)' : 'rgb(0, 122, 255, 0.2)'}
-                    strokeColor={darkMode ? 'rgb(10, 132, 255)' : 'rgb(0, 122, 255)'}
-                    strokeLinecap="butt"
-                />
-            </div>
+    return (
+        currentRouteInformation.status === 'FETCHING' && (
+            <StyledProgressBar>
+                <Track darkMode={darkMode} />
+                <Bar darkMode={darkMode} progress={currentRouteInformation.progress} />
+            </StyledProgressBar>
         )
-    }
-
-    return null
+    )
 }
