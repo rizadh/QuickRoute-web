d51c5507d359a2b825f45da50a062923018123fa
Rizadh Nizam <me@rizadh.com>
2020-02-16 23:44:29 -0500

    Move inner map components to MapContainer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { ProgressBar } from '../components/ProgressBar'
-import { useCompactMode } from '../hooks/useCompactMode'
 import { AppState } from '../redux/state'
 import { WaypointEditor } from './editor/WaypointEditor'
-import { MapButtons } from './map/MapButtons'
 import { MapContainer } from './map/MapContainer'
-import { MapView } from './map/MapView'
 
 const Container = styled.div`
     display: flex;
@@ -19,15 +16,11 @@ const Container = styled.div`
 
 export const App = hot(module)(() => {
     const editorIsHidden = useSelector((state: AppState) => state.editorIsHidden)
-    const compactMode = useCompactMode()
 
     return (
         <Container>
             {!editorIsHidden && <WaypointEditor />}
-            <MapContainer hidden={compactMode && !editorIsHidden}>
-                <MapView />
-                <MapButtons />
-            </MapContainer>
+            <MapContainer />
             <ProgressBar />
         </Container>
     )
--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -1,6 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
 import styled, { css } from 'styled-components'
+import { useCompactMode } from '../../hooks/useCompactMode'
+import { AppState } from '../../redux/state'
+import { MapButtons } from './MapButtons'
+import { MapView } from './MapView'
 
-export const MapContainer = styled.div<{ hidden: boolean }>`
+const Container = styled.div<{ hidden: boolean }>`
     position: relative;
     display: flex;
     flex-grow: 1;
@@ -12,3 +18,15 @@ export const MapContainer = styled.div<{ hidden: boolean }>`
             display: none;
         `}
 `
+
+export const MapContainer = () => {
+    const editorIsHidden = useSelector((state: AppState) => state.editorIsHidden)
+    const compactMode = useCompactMode()
+
+    return (
+        <Container hidden={compactMode && !editorIsHidden}>
+            <MapView />
+            <MapButtons />
+        </Container>
+    )
+}
