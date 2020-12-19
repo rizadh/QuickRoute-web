700d3c65effdd7a56c6f17a620329e10862df9fc
Rizadh Nizam <me@rizadh.com>
2020-12-19 01:59:35 -0500

    Revamp export page
    - Add parameters to show dividers, change orientation and set columns
    - Improve rendering logic to automatically shrink text when it cannot fit
    - Improve spacing around addresses

--- src/components/editor/panes/ExportPane.tsx
+++ src/components/editor/panes/ExportPane.tsx
@@ -11,77 +11,52 @@ import { AppState } from '../../../redux/state'
 import { saveAs } from 'file-saver'
 import { useInput } from '../../../hooks/useInput'
 
-const DEFAULT_ROWS_PER_PAGE = 25
+const DEFAULT_ROWS_PER_PAGE = 40
 const DEFAULT_COLUMNS_PER_PAGE = 2
-const DEFAULT_FONT_SIZE = 16
+const DEFAULT_FONT_SIZE = 14
 
 export const ExportPane = () => {
     const waypoints = useSelector((state: AppState) => state.waypoints)
+    const [useLandscape, setUseLandscape] = useState(false)
     const [orderByAddress, setOrderByAddress] = useState(false)
-    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const [drawDividers, setDrawDividers] = useState(false)
+    const handleOrderByAddressChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setOrderByAddress(e.currentTarget.checked)
+        // if (!e.currentTarget.checked) setDrawDividers(false)
+    }, [])
+    const handleUseLandscapeChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setUseLandscape(e.currentTarget.checked)
+    }, [])
+    const handleDrawDividersChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setDrawDividers(e.currentTarget.checked)
     }, [])
     const { value: rowsFieldValue, props: rowsFieldProps, valueIsValid: rowsIsValid } = useInput({
+        predicate: value => value.length === 0 || !isNaN(parseInt(value)),
+    })
+    const { value: columnsFieldValue, props: columnsFieldProps, valueIsValid: columnsIsValid } = useInput({
         predicate: value => value.length === 0 || !isNaN(parseInt(value)),
     })
     const { value: fontSizeFieldValue, props: fontSizeFieldProps, valueIsValid: fontSizeIsValid } = useInput({
         predicate: value => value.length === 0 || !isNaN(parseInt(value)),
     })
 
-    const canDownload = rowsIsValid && fontSizeIsValid
+    const fieldsAreValid = rowsIsValid && columnsIsValid && fontSizeIsValid
 
-    const downloadPdf = useCallback(async () => {
-        const PAGE_MARGIN = 72 / 4
-        const LIST_INDENT = 72 / 2
-
-        const rowsPerPage = parseInt(rowsFieldValue) || DEFAULT_ROWS_PER_PAGE
-        const columnsPerPage = DEFAULT_COLUMNS_PER_PAGE
-        const fontSize = parseInt(fontSizeFieldValue) || DEFAULT_FONT_SIZE
-        const waypointsPerPage = rowsPerPage * columnsPerPage
-
-        const document = await PDFDocument.create()
-        const font = await document.embedFont(StandardFonts.HelveticaBold)
-        const pages = range(0, Math.ceil(waypoints.length / waypointsPerPage)).map(() =>
-            document.addPage([PageSizes.Letter[1], PageSizes.Letter[0]]),
-        )
-
-        const indexedWaypoints = waypoints.map((waypoint, index) => ({ waypoint, index }))
-        if (orderByAddress) indexedWaypoints.sort((a, b) => a.waypoint.address.localeCompare(b.waypoint.address))
-
-        indexedWaypoints.forEach(({ waypoint, index: originalIndex }, index) => {
-            const page = pages[Math.floor(index / waypointsPerPage)]
-
-            const rowIndex = index % rowsPerPage
-            const columnIndex = Math.floor((index / rowsPerPage) % columnsPerPage)
-
-            const { height: pageHeight, width: pageWidth } = page.getSize()
-
-            const effectivePageHeight = pageHeight - 2 * PAGE_MARGIN
-            const rowPadding = effectivePageHeight / rowsPerPage - font.heightAtSize(fontSize)
-            const rowHeight = effectivePageHeight / rowsPerPage
-            const rowPosition = pageHeight - PAGE_MARGIN - (rowIndex + 1) * rowHeight
-
-            const effectivePageWidth = pageWidth - 2 * PAGE_MARGIN
-            const columnWidth = effectivePageWidth / columnsPerPage
-            const columnPosition = PAGE_MARGIN + columnIndex * columnWidth
-
-            page.drawText(`${originalIndex + 1}.`, {
-                x: columnPosition,
-                y: rowPosition + rowPadding / 2,
-                size: fontSize,
-                font,
-            })
-            page.drawText(waypoint.address, {
-                x: columnPosition + LIST_INDENT,
-                y: rowPosition + rowPadding / 2,
-                size: fontSize,
-                font,
-            })
-        })
-
-        const dateString = new Date().toISOString().split('T')[0].replace(/-/g, '')
-        saveAs(new Blob([await document.save()]), `waypoints-${dateString}.pdf`)
-    }, [waypoints, orderByAddress, rowsFieldValue, fontSizeFieldValue])
+    const downloadPdf = useCallback(
+        () =>
+            generateAndDownloadPdf(
+                waypoints.map(({ address }) => address),
+                {
+                    rowsPerPage: parseInt(rowsFieldValue, 10) || DEFAULT_ROWS_PER_PAGE,
+                    columnsPerPage: parseInt(columnsFieldValue, 10) || DEFAULT_COLUMNS_PER_PAGE,
+                    preferredfontSize: parseInt(fontSizeFieldValue, 10) || DEFAULT_FONT_SIZE,
+                    orderByAddress,
+                    useLandscape,
+                    drawDividers,
+                },
+            ),
+        [waypoints, rowsFieldValue, columnsFieldValue, fontSizeFieldValue, orderByAddress, useLandscape, drawDividers],
+    )
 
     return (
         <>
@@ -92,18 +67,38 @@ export const ExportPane = () => {
                     <FixedWidthInput placeholder={DEFAULT_ROWS_PER_PAGE.toString()} {...rowsFieldProps} />
                 </InputRow>
                 <InputRow>
+                    <Alert>Columns per page</Alert>
+                    <Spacer />
+                    <FixedWidthInput placeholder={DEFAULT_COLUMNS_PER_PAGE.toString()} {...columnsFieldProps} />
+                </InputRow>
+                <InputRow>
                     <Alert>Font size</Alert>
                     <Spacer />
                     <FixedWidthInput placeholder={DEFAULT_FONT_SIZE.toString()} {...fontSizeFieldProps} />
                 </InputRow>
                 <InputRow>
-                    <Alert>List addresses in alphabetical order</Alert>
+                    <Alert>Use landscape orientation</Alert>
                     <Spacer />
-                    <Checkbox type="checkbox" checked={orderByAddress} onChange={handleChange}></Checkbox>
+                    <Checkbox type="checkbox" checked={useLandscape} onChange={handleUseLandscapeChange}></Checkbox>
+                </InputRow>
+                <InputRow>
+                    <Alert>Sort by house number</Alert>
+                    <Spacer />
+                    <Checkbox type="checkbox" checked={orderByAddress} onChange={handleOrderByAddressChange}></Checkbox>
+                </InputRow>
+                <InputRow>
+                    <Alert>Draw dividers</Alert>
+                    <Spacer />
+                    <Checkbox
+                        type="checkbox"
+                        checked={drawDividers}
+                        disabled={!orderByAddress}
+                        onChange={handleDrawDividersChange}
+                    ></Checkbox>
                 </InputRow>
             </Body>
             <Footer>
-                <Button variant={Variant.Primary} onClick={downloadPdf} disabled={!canDownload}>
+                <Button variant={Variant.Primary} onClick={downloadPdf} disabled={!fieldsAreValid}>
                     <i className="fas fa-fw fa-file-download" /> Download
                 </Button>
             </Footer>
@@ -120,3 +115,74 @@ const FixedWidthInput = styled(Input)`
     line-height: 1px;
     text-align: right;
 `
+
+const PAGE_MARGIN = 72 / 4
+
+type GeneratePdfOptions = {
+    rowsPerPage: number
+    columnsPerPage: number
+    preferredfontSize: number
+    orderByAddress: boolean
+    useLandscape: boolean
+    drawDividers: boolean
+}
+
+async function generateAndDownloadPdf(
+    addresses: string[],
+    { rowsPerPage, columnsPerPage, preferredfontSize, orderByAddress, useLandscape, drawDividers }: GeneratePdfOptions,
+) {
+    const waypointsPerPage = rowsPerPage * columnsPerPage
+
+    const document = await PDFDocument.create()
+    const font = await document.embedFont(StandardFonts.HelveticaBold)
+    const [pageWidth, pageHeight] = useLandscape ? [...PageSizes.Letter].reverse() : PageSizes.Letter
+    const rowHeight = (pageHeight - 2 * PAGE_MARGIN) / rowsPerPage
+    const baselineOffset = rowHeight - font.heightAtSize(preferredfontSize)
+    const columnWidth = (pageWidth - (1 + columnsPerPage) * PAGE_MARGIN) / columnsPerPage
+    const pages = range(0, Math.ceil(addresses.length / waypointsPerPage)).map(() =>
+        document.addPage([pageWidth, pageHeight]),
+    )
+    const indexTextWidth = font.widthOfTextAtSize('00.', preferredfontSize)
+    const textHeight = font.heightAtSize(preferredfontSize)
+    const addressIndent = indexTextWidth + textHeight / 2
+    const availableAddressWidth = columnWidth - addressIndent
+
+    const indexedAddresses = addresses.map((address, index) => ({ address, index }))
+    if (orderByAddress) indexedAddresses.sort((a, b) => a.address.localeCompare(b.address))
+
+    let lastCharacter: string
+    indexedAddresses.forEach(({ address, index: originalIndex }, index) => {
+        const rowIndex = index % rowsPerPage
+        const columnIndex = Math.floor(index / rowsPerPage) % columnsPerPage
+        const rowPosition = pageHeight - PAGE_MARGIN - (rowIndex + 1) * rowHeight
+        const columnPosition = PAGE_MARGIN + columnIndex * (columnWidth + PAGE_MARGIN)
+
+        const fullsizeAddressWidth = font.widthOfTextAtSize(address, preferredfontSize)
+        const fontScaleFactor = Math.min(availableAddressWidth / fullsizeAddressWidth, 1)
+
+        const page = pages[Math.floor(index / waypointsPerPage)]
+        page.drawText(`${originalIndex + 1}.`, {
+            x: columnPosition,
+            y: rowPosition + baselineOffset,
+            size: preferredfontSize,
+            font,
+        })
+        page.drawText(address, {
+            x: columnPosition + addressIndent,
+            y: rowPosition + baselineOffset,
+            size: preferredfontSize * fontScaleFactor,
+            font,
+        })
+        if (orderByAddress && drawDividers && lastCharacter && lastCharacter !== address[0]) {
+            page.drawLine({
+                start: { x: columnPosition, y: rowPosition + rowHeight },
+                end: { x: columnPosition + columnWidth, y: rowPosition + rowHeight },
+            })
+        }
+
+        lastCharacter = address[0]
+    })
+
+    const dateString = new Date().toISOString().split('T')[0].replace(/-/g, '')
+    saveAs(new Blob([await document.save()]), `waypoints-${dateString}.pdf`)
+}
