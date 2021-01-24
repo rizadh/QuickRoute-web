c6dd78a4495d51fa2e806e9f1848f0f7d438b1a8
Rizadh Nizam <me@rizadh.com>
2021-01-23 22:27:57 -0500

    Update app version

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { GetTokenQuery, GetTokenQueryVariables } from './generated/graphql'
 import { GetToken } from './queries'
 import store from './redux/store'
 
-export const appVersion = '2021.01.17'
+export const appVersion = '2021.01.23'
 export const apolloClient = new ApolloClient({ uri: 'https://api.quickroute.rizadh.com/graphql' })
 
 // tslint:disable-next-line:no-var-requires
